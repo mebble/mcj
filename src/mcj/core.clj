0ed6427d222857eae11c6636fb1034d85c981f51
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-09-17 21:09:05 +0530

    refactor either alias

--- src/mcj/core.clj
+++ src/mcj/core.clj
@@ -1,6 +1,6 @@
 (ns mcj.core
   (:gen-class)
-  (:require [cats.monad.either :as either]
+  (:require [cats.monad.either :as e]
             [clojure.string :as s]))
 
 (defn -main
@@ -12,32 +12,32 @@
 
 (def operations #{:add :sub :mul :div})
 
-(defn- parse-arg [arg-str] (try (either/right (Double/parseDouble arg-str))
+(defn- parse-arg [arg-str] (try (e/right (Double/parseDouble arg-str))
                                 (catch NumberFormatException _e
-                                  (either/left (str "Invalid argument " arg-str)))))
+                                  (e/left (str "Invalid argument " arg-str)))))
 
 (defn get-command
   [op-str arg1-str arg2-str]
   (let [op (keyword op-str)]
     (if (contains? operations op)
       (if (some s/blank? [arg1-str arg2-str])
-        (either/left "Insufficient arguments")
+        (e/left "Insufficient arguments")
         (let [arg1 (parse-arg arg1-str)
               arg2 (parse-arg arg2-str)
-              left (either/first-left [arg1 arg2])]
+              left (e/first-left [arg1 arg2])]
           (if (nil? left)
-            (either/right {:op op
-                           :arg1 (either/branch-right arg1 identity)
-                           :arg2 (either/branch-right arg2 identity)})
+            (e/right {:op op
+                           :arg1 (e/branch-right arg1 identity)
+                           :arg2 (e/branch-right arg2 identity)})
             left)))
-      (either/left (str "Unknown command " op-str)))))
+      (e/left (str "Unknown command " op-str)))))
 
 (defn execute
   [{ op :op, arg1 :arg1, arg2 :arg2 }]
   (case op
-    :add (either/right (+ arg1 arg2))
-    :sub (either/right (- arg1 arg2))
-    :mul (either/right (* arg1 arg2))
+    :add (e/right (+ arg1 arg2))
+    :sub (e/right (- arg1 arg2))
+    :mul (e/right (* arg1 arg2))
     :div (if (= arg2 0)
-           (either/left "Can't divide by zero")
-           (either/right (/ arg1 arg2)))))
+           (e/left "Can't divide by zero")
+           (e/right (/ arg1 arg2)))))
--- test/mcj/core_test.clj
+++ test/mcj/core_test.clj
@@ -1,38 +1,38 @@
 (ns mcj.core-test
   (:require [clojure.test :refer :all]
             [mcj.core :refer :all]
-            [cats.monad.either :as either]))
+            [cats.monad.either :as e]))
 
 (deftest test-get-command
   (testing "Basic happy path"
-    (let [get-expected (fn [op] (either/right {:op op, :arg1 2.0, :arg2 3.0}))]
+    (let [get-expected (fn [op] (e/right {:op op, :arg1 2.0, :arg2 3.0}))]
       (is (= (get-expected :add) (get-command "add" "2" "3")))
       (is (= (get-expected :sub) (get-command "sub" "2" "3")))
       (is (= (get-expected :mul) (get-command "mul" "2" "3")))
       (is (= (get-expected :div) (get-command "div" "2" "3")))))
 
   (testing "Negative arguments"
-    (is (= (either/right {:op :add, :arg1 -2.0, :arg2 -3.0})
+    (is (= (e/right {:op :add, :arg1 -2.0, :arg2 -3.0})
            (get-command "add" "-2" "-3"))))
 
   (testing "Fractional arguments"
-    (is (= (either/right {:op :add, :arg1 2.5, :arg2 3.2})
+    (is (= (e/right {:op :add, :arg1 2.5, :arg2 3.2})
            (get-command "add" "2.5" "3.2"))))
 
   (testing "Unknown command"
-    (is (= (either/left "Unknown command foo")
+    (is (= (e/left "Unknown command foo")
            (get-command "foo" "2" "3"))))
 
   (testing "Invalid argument"
-    (is (= (either/left "Invalid argument abc")
+    (is (= (e/left "Invalid argument abc")
            (get-command "add" "abc" "3")))
-    (is (= (either/left "Invalid argument def")
+    (is (= (e/left "Invalid argument def")
            (get-command "add" "2" "def")))
-    (is (= (either/left "Invalid argument abc")
+    (is (= (e/left "Invalid argument abc")
            (get-command "add" "abc" "def"))))
 
   (testing "Missing argument"
-    (let [expected (either/left "Insufficient arguments")]
+    (let [expected (e/left "Insufficient arguments")]
       (testing "arg1"
         (is (= expected (get-command "add" nil "3")))
         (is (= expected (get-command "add" "" "3")))
@@ -45,12 +45,12 @@
 
 (deftest test-execute
   (testing "Happy path"
-    (is (= (either/right 5) (execute {:op :add, :arg1 2, :arg2 3 })))
-    (is (= (either/right -1) (execute {:op :sub, :arg1 2, :arg2 3})))
-    (is (= (either/right 6) (execute {:op :mul, :arg1 2, :arg2 3})))
-    (is (= (either/right 2/3) (execute {:op :div, :arg1 2, :arg2 3}))))
+    (is (= (e/right 5) (execute {:op :add, :arg1 2, :arg2 3 })))
+    (is (= (e/right -1) (execute {:op :sub, :arg1 2, :arg2 3})))
+    (is (= (e/right 6) (execute {:op :mul, :arg1 2, :arg2 3})))
+    (is (= (e/right 2/3) (execute {:op :div, :arg1 2, :arg2 3}))))
 
   (testing "Argument is zero"
-    (is (= (either/right 0) (execute {:op :mul, :arg1 0, :arg2 1})))
-    (is (= (either/right 0) (execute {:op :div, :arg1 0, :arg2 1})))
-    (is (= (either/left "Can't divide by zero") (execute {:op :div, :arg1 1, :arg2 0})))))
+    (is (= (e/right 0) (execute {:op :mul, :arg1 0, :arg2 1})))
+    (is (= (e/right 0) (execute {:op :div, :arg1 0, :arg2 1})))
+    (is (= (e/left "Can't divide by zero") (execute {:op :div, :arg1 1, :arg2 0})))))
