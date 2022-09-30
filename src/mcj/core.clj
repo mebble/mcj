b573b0636131a0251696f8780382c429a5cfa50e
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-09-30 20:44:38 +0530

    add informational arg handling to integration

--- src/mcj/core.clj
+++ src/mcj/core.clj
@@ -1,22 +1,22 @@
 (ns mcj.core
   (:gen-class)
   (:require [cats.core :as c]
-            [mcj.argv :refer [parse-argv read-dot]]
+            [mcj.argv :refer [parse-argv read-dot break-out]]
             [mcj.command :refer [parse-command execute]]
             [mcj.utils :refer [buildtime-env]]))
 
-(def project-url (buildtime-env :PROJECT_URL))
-(def app-version (buildtime-env :APP_VERSION))
+(def configs {:PROJECT_URL (buildtime-env :PROJECT_URL)
+              :APP_VERSION (buildtime-env :APP_VERSION)})
 
 (defn -main
   "Execute arithmetic expression from command line arguments"
   [& argv]
   (as-> argv x
-       (parse-argv x)
-       (c/bind x #(->> %
-                       :cmd-str
-                       (read-dot read-line)
-                       (apply parse-command)))
-       (c/bind x execute)
-       (c/extract x)
-       (println x)))
+    (parse-argv x)
+    (c/bind x #(break-out configs %))
+    (c/bind x #(->> %
+                    (read-dot read-line)
+                    (apply parse-command)))
+    (c/bind x execute)
+    (c/extract x)
+    (println x)))
--- test/mcj/core_test.clj
+++ test/mcj/core_test.clj
@@ -6,9 +6,14 @@
   (let [mock-out (fn [f] (with-out-str (f)))
         mock-in-out (fn [f] (with-in-str "10" (mock-out f)))]
     (testing "Happy path"
-      (is (= "5.0\n" (mock-out #(-main "add" "2" "3"))))
-      (is (= "5.0\n" (mock-out #(-main "add" "2" "3" "extra" "argv" "items"))))
-      (is (= "12.0\n" (mock-in-out #(-main "add" "2" ".")))))
+      (testing "Information argv"
+        (is (= "vx.y.z\n" (mock-out #(-main "-v"))))
+        (is (= "Help output\n" (mock-out #(-main "-h")))))
+
+      (testing "Arithmetic command argv"
+        (is (= "5.0\n" (mock-out #(-main "add" "2" "3"))))
+        (is (= "5.0\n" (mock-out #(-main "add" "2" "3" "extra" "argv" "items"))))
+        (is (= "12.0\n" (mock-in-out #(-main "add" "2" "."))))))
 
     (testing "Sad path"
       (is (= "No command given\n" (mock-out #(-main))))
