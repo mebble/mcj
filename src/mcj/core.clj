590f5f926fa6b44b549a3bf8c91860966e6d4ceb
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-10-03 00:31:10 +0530

    [refactor]

--- src/mcj/core.clj
+++ src/mcj/core.clj
@@ -5,13 +5,14 @@
             [mcj.command :refer [parse-command execute]]
             [mcj.utils :refer [buildtime-env]]))
 
-(def configs {:PROJECT_URL (buildtime-env :PROJECT_URL)
-              :APP_VERSION (buildtime-env :APP_VERSION)})
+(def ^:private configs {:PROJECT_URL (buildtime-env :PROJECT_URL)
+                        :APP_VERSION (buildtime-env :APP_VERSION)})
 
-(defn monadic-pipeline [argv]
+(defn- monadic-pipeline [argv]
   (c/->>= (parse-argv argv)
           (break-out configs)
-          (#(->> (read-dot read-line %)
+          (#(->> %
+                 (read-dot read-line)
                  (apply parse-command)))
           execute))
 
