a918b658455aca924b252f5e287965bd44851ec7
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-10-08 15:55:40 +0530

    hard code rounding output to 4 in integration

--- src/mcj/core.clj
+++ src/mcj/core.clj
@@ -3,18 +3,18 @@
   (:require [cats.core :as c]
             [mcj.argv :refer [parse-argv read-dot break-out]]
             [mcj.command :refer [parse-command execute]]
-            [mcj.utils :refer [buildtime-env]]))
+            [mcj.utils :refer [buildtime-env round-to]]))
 
 (def ^:private configs {:PROJECT_URL (buildtime-env :PROJECT_URL)
                         :APP_VERSION (buildtime-env :APP_VERSION)})
 
 (defn- monadic-pipeline [argv]
-  (c/->>= (parse-argv argv)
-          (break-out configs)
-          (#(->> %
-                 (read-dot read-line)
-                 (apply parse-command)))
-          execute))
+  (c/fmap #(round-to 4 %) (c/->>= (parse-argv argv)
+                                  (break-out configs)
+                                  (#(->> %
+                                         (read-dot read-line)
+                                         (apply parse-command)))
+                                  execute)))
 
 (defn -main
   "Execute arithmetic expression from command line arguments"
--- test/mcj/core_test.clj
+++ test/mcj/core_test.clj
@@ -6,14 +6,20 @@
   (let [mock-out (fn [f] (with-out-str (f)))
         mock-in-out (fn [f] (with-in-str "10" (mock-out f)))]
     (testing "Happy path"
-      (testing "Information argv"
+
+      (testing "Information"
         (is (= "vx.y.z\n" (mock-out #(-main "-v"))))
         (is (= "Help output\n" (mock-out #(-main "-h")))))
 
-      (testing "Arithmetic command argv"
-        (is (= "5.0\n" (mock-out #(-main "add" "2" "3"))))
-        (is (= "5.0\n" (mock-out #(-main "add" "2" "3" "extra" "argv" "items"))))
-        (is (= "12.0\n" (mock-in-out #(-main "add" "2" "."))))))
+      (testing "Arithmetic command"
+        (testing "Basic"
+          (is (= "5.0\n" (mock-out #(-main "add" "2" "3"))))
+          (is (= "5.0\n" (mock-out #(-main "add" "2" "3" "extra" "argv" "items"))))
+          (is (= "12.0\n" (mock-in-out #(-main "add" "2" ".")))))
+
+        (testing "Formatted output"
+          (is (= "0.6667\n" (mock-out #(-main "div" "2" "3" "-d" "4"))))
+          (is (= "1.3333\n" (mock-out #(-main "div" "4" "3" "-d" "4")))))))
 
     (testing "Sad path"
       (is (= "No command given\n" (mock-out #(-main))))
